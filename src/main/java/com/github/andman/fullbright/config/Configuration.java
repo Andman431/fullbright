3e3a32540195ca4dcba0af7c158432426f879a1f
Andman <87638167+Andman431@users.noreply.github.com>
2023-06-04 11:45:37 +0200

    updating

--- src/main/java/com/github/andman/fullbright/config/Configuration.java
+++ src/main/java/com/github/andman/fullbright/config/Configuration.java
@@ -6,11 +6,9 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "full_bright")
 public class Configuration implements ConfigData {
-    @ConfigEntry.Gui.PrefixText
     @ConfigEntry.Gui.Tooltip(count = 2)
     public boolean toggled = true;
 
-    @ConfigEntry.Gui.PrefixText
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.BoundedDiscrete(min = 1, max = 50)
     public float gamma = 15f;
